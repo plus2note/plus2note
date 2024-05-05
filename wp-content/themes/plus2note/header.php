a0fd914db755b27d48ad5fe118c92a28c1a0b1e5
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-05-06 03:18:41 +0545

    create wordpress template parts...

--- wp-content/themes/plus2note/header.php
+++ wp-content/themes/plus2note/header.php
@@ -4,7 +4,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Plus2Note - Your ultimate destination for exam preparation, study notes, routine notices, exam notices, and class notes for Nepali Class 11 and 12 students. Get comprehensive notes for all subjects.">
-    <meta name="keywords" content="Plus2Note, exam preparation, study notes, routine notices, exam notices, class notes, Nepali Class 11, Nepali Class 12, notes, subjects">
+    <meta name="keywords" content="Plus2Note, exam preparation, study notes, routine notices, exam notices, class notes, Nepali Class 11, Class 11 Notes, Class 12 Notes, Nepali Class 12, notes, subjects">
     <meta name="author" content="Pradip Chaudhary">
     <meta name="robots" content="index, follow">
     <meta name="theme-color" content="#0073e6">
@@ -18,22 +18,21 @@
 <body <?php body_class(); ?>>
   
 
-
+    <!-- Header Section  -->
     <header class="page-header">	    
-	<div class="container plus2note_header">
-		<h1 class="page-logo">
-			<a class="site-title pt-lg-4 mb-0" href="index.html">plus2note</a>
-		</h1>
-		<nav class="navbar">
-		<?php
-			wp_nav_menu(array(
-			'theme_location' => 'primary',
-			'menu_class' => 'navbar-menu',
-			));
-			?>
+        <div class="container plus2note_header">
+            <h1 class="plus2note_logo">
+                <?php ?>
+                <a class="site-title pt-lg-4 mb-0" href="index.html"><?php bloginfo('name'); ?></a>
+            </h1>
+            <nav class="navbar">
+            <?php
+                wp_nav_menu(array(
+                'theme_location' => 'primary',
+                'menu_class' => 'navbar-menu',
+                ));
+                ?>
 
-		</nav>
-	</div>
-        
-	    
+            </nav>
+        </div>	    
     </header>
---
+++ wp-content/themes/plus2note/template-parts/content-archive.php
@@ -0,0 +1,13 @@
+<div class="container">
+      <div class="post mb-5">
+            <div class="media">
+                  <img class="mr-3 img-fluid post-thumb d-none d-md-flex" src="images/thumbs/1.jpg" alt="image">
+                  <div class="media-body">
+                        <h3 class="title mb-1"><a href="post.html"><?php the_title(); ?></a></h3>
+                        <div class="meta mb-1"><span class="date">Published 2 days ago</span><span class="time">5 min read</span><span class="comment"><a href="#">8 comments</a></span></div>
+                        <div class="intro"><?php the_excerpt(); ?></div>
+                        <a class="more-link" href="post.html">Read more &rarr;</a>
+                  </div><!--//media-body-->
+            </div><!--//media-->
+      </div>
+</div>
