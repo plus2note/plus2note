e4a34aee6810dc72e91d6d887e16b31d7f9f1a95
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-05-17 23:14:33 +0545

    upd

--- wp-content/themes/plus2note/template-parts/content-archive.php
+++ wp-content/themes/plus2note/template-parts/content-archive.php
@@ -1,7 +1,14 @@
 <div class="container">
       <div class="post mb-5">
             <div class="media">
-                  <img class="mr-3 img-fluid post-thumb d-none d-md-flex" src="images/thumbs/1.jpg" alt="image">
+                  <?php if(! has_post_thumbnail()){ ?>
+                        <img class="mr-3 img-fluid post-thumb d-none d-md-flex" src="<?php bloginfo('template_directory'); ?>/assets/images/default-image.png" alt="image">
+                        <?php } else { ?>
+                              <img class="mr-3 img-fluid post-thumb d-none d-md-flex" src="<?php the_post_thumbnail_url() ?>" alt="image">
+                        <?php
+                        }
+                  ?>
+                  <img class="mr-3 img-fluid post-thumb d-none d-md-flex" src="<?php the_post_thumbnail_url() ?>" alt="image">
                   <div class="media-body">
                         <h3 class="title mb-1"><a href="post.html"><?php the_title(); ?></a></h3>
                         <div class="meta mb-1"><span class="date"><?php the_date(); ?></span><span class="comment"><a href="#"><?php comments_number(); ?></a></span></div>
